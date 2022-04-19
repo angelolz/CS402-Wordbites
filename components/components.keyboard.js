923f1fb39eaa054ce19e9e85cfbe9b62d9187d07
shadowbladerx1 <danie4411@gmail.com>
2022-04-19 15:56:32 -0600

    Placeholder for keyboard

---
+++ components/components.keyboard.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+const KeyboardComponent = (props) => {
+    var keyboard = 
+    <View>
+        <Text>Hello world!</Text>
+    </View>;
+    return keyboard;
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default KeyboardComponent;
