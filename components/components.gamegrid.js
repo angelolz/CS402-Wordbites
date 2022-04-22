0e959abce531bfffa5f3969601c900923383c6a6
shadowbladerx1 <danie4411@gmail.com>
2022-04-22 14:37:46 -0600

    Prototype of gameboard

--- components/components.gamegrid.js
+++ components/components.gamegrid.js
@@ -6,30 +6,37 @@ const GameGridComponent = (props) => {
     const [wordLength, setWordLength] = useState(5);
     const [guesses, setGuesses] = useState(6);
 
-    const row = () => {
-        var box;
-        for (var i = 0; i < wordLength; i++) {
-            box += <View style={styles.box}>
-                {/* <Text>i</Text> */}
-            </View>
+    const boxes = (rowNum) => {
+        let box = [];
+
+        for(let i = 0; i < wordLength; i++){
+            box.push(
+                <View key={`${rowNum}:${i}`} style={styles.box}>
+                    <Text>i</Text>
+                </View>
+            );
         }
 
         return box;
     }
 
-    const grid = () => {
-        var rows;
-        for (var i = 0; i < guesses; i++) {
-            rows += row()
+    const rows = () => {
+        let row = [];
+
+        for(let i = 0; i < guesses; i++){
+            row.push(
+                <View key={`${i}`} style={styles.row}>
+                    {boxes(i)}
+                </View>
+            );
         }
 
-        return rows;
+        return row;
     }
-
 
     return (
         <View style={styles.container}>
-            {grid()}
+            {rows()}
         </View>
     );
 }
@@ -39,11 +46,16 @@ const styles = StyleSheet.create({
         width: "100%",
         paddingHorizontal: 5
     },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
+    },
     box: {
         borderColor: "black",
         borderWidth: 5,
         color: "black",
         padding: 10
+
     }
 });
 
