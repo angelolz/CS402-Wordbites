8aed9eae82b7bf726e5d66e539a97f95a5646e75
shadowbladerx1 <danie4411@gmail.com>
2022-05-04 00:45:44 -0600

    Added persistence to stats and settings

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { RootSiblingParent } from 'react-native-root-siblings';
 
 import MainMenu from './src/views/MainMenu';
 import GameBoard from './src/views/GameBoard';
 import { emptyStats, Screen } from './src/constants/Constants';
 import StatsScreen from './src/views/Stats';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 var wordBites = () => {
     const [curView, setView] = useState(Screen.MAIN_MENU);
@@ -16,6 +18,57 @@ var wordBites = () => {
     const [theme, changeTheme] = useState("light")
     const [colorblind, toggleColorblind] = useState(false);
     const [swapKeys, toggleSwap] = useState(false)
+
+    const [initialRender, setInitialRender] = useState(true);
+    const [loadedInitialSettings, setLoadedInitialSettings] = useState(false);
+
+    useEffect(async () => {
+        try{
+            // Skip the first render
+            if(initialRender){
+                setInitialRender(false);
+                return;
+            }
+
+            // Skip if change was due to saved settings
+            if(!loadedInitialSettings){
+                return;
+            }
+            await AsyncStorage.setItem('@hardMode', JSON.stringify(hardMode));
+            await AsyncStorage.setItem('@theme', JSON.stringify(theme));
+            await AsyncStorage.setItem('@colorblind', JSON.stringify(colorblind));
+            await AsyncStorage.setItem('@swapKeys', JSON.stringify(swapKeys));
+            await AsyncStorage.setItem('@stats', JSON.stringify(stats));
+        }
+        catch(e){
+
+        }
+    }, [hardMode, theme, colorblind, swapKeys, stats]);
+    
+    // Load settings on startup
+    useEffect(async () => {
+        try{
+            if(!loadedInitialSettings){
+                const hardModeStorage = await AsyncStorage.getItem('@hardMode');
+                const themeStorage = await AsyncStorage.getItem('@theme');
+                const colorblindStorage = await AsyncStorage.getItem('@colorblind');
+                const swapKeysStorage = await AsyncStorage.getItem('@swapKeys');
+                const statsStorage = await AsyncStorage.getItem('@stats');
+                toggleHardMode(JSON.parse(hardModeStorage));
+                changeTheme(JSON.parse(themeStorage));
+                toggleColorblind(JSON.parse(colorblindStorage));
+                toggleSwap(JSON.parse(swapKeysStorage));
+                if(JSON.parse(statsStorage)){
+                    updateStats(JSON.parse(statsStorage));
+                }
+                setLoadedInitialSettings(true);
+            }
+        }
+        catch(e) {
+
+        }
+    }, []);
+
 
     function view() {
         switch (curView) {
